5e302162eec22c5397c85ef1db258776c64ffb7b
amber1888 <1175276195@qq.com>
2025-06-20 19:26:01 +0800

    插件文件修改

--- plugins.v2/esb/__init__.py
+++ plugins.v2/esb/__init__.py
@@ -148,8 +148,9 @@ class Esb(_PluginBase):
             if res:
                 ret_json = res.json()
                 response = ""
-                for album in ret_json["result"]:
-                    response += "%s:%s\n" % (album["album"], album["title"].decode("utf8"))
+                albums = json.loads(ret_json["result"])
+                for album in albums:
+                    response += "%s:%s\n" % (album["album_id"], album["title"].decode("utf8"))
                 logger.info(f"返回：{response}")
                 self.post_message(channel=channel, title=response, userid=userid)
         except Exception as e:
